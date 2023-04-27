34f2a6f1f410f6202e3abab120c82d9396e34356
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-04-27 08:17:31 +0100

    add code and tests for part 1 of fillSquare()

--- javascript-frontend-katas/__tests__/fillSquare.test.js
+++ javascript-frontend-katas/__tests__/fillSquare.test.js
@@ -1,7 +1,9 @@
-const { fillSquare } = require('../katas/fillSquare/fillSquare');
+const { fillSquare } = require("../katas/fillSquare/fillSquare");
 
-describe('fillSquare()', () => {
-  it('', () => {
-    // first test here
+describe("fillSquare()", () => {
+  it("returns an array of a 3x3 square with null filling the blank spaces in the sub arrays", () => {
+    expect(fillSquare([[1, 2, 3], [1, 2], [1]])).toEqual(
+      [[1, 2, 3], [1, 2, null], [1, null, null]]
+    );
   });
 });
--- javascript-frontend-katas/katas/fillSquare/fillSquare.js
+++ javascript-frontend-katas/katas/fillSquare/fillSquare.js
@@ -1,6 +1,27 @@
 // Please do not change the name of this function
-const fillSquare = (arr) => {
-  // Your code here
-}
+const fillSquare = arr => {
 
-module.exports = { fillSquare }
+  // REFACTOR IDEA manually iterating could improve efficiency as the current code iterates over the array multiple times (map, mex, indexOf, additional loops...)
+
+  const square = [];
+  const lengths = arr.map(array => array.length);
+  const maxLength = lengths[lengths.indexOf(Math.max(...lengths))];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length === maxLength) {
+      square.push(arr[i]);
+    } else {
+      const a = [];
+      for (let j = 0; j < maxLength; j++) {
+        if (!arr[i][j]) {
+          a.push(null);
+        } else a.push(arr[i][j]);
+      }
+
+      square.push(a);
+    }
+  }
+  return square;
+};
+
+module.exports = { fillSquare };
