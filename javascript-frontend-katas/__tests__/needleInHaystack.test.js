349c2d483d3b67a6d199e78cea28ce5ae248a0f5
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-04-28 19:38:38 +0100

    refactor to sort output alphabetically needleInHaystack

--- javascript-frontend-katas/__tests__/needleInHaystack.test.js
+++ javascript-frontend-katas/__tests__/needleInHaystack.test.js
@@ -94,4 +94,33 @@ describe("needleInHaystack()", () => {
       "contactDetails.address.postcode"
     ]);
   });
+  it("Returns the correct path for multiple instances sorted alphabetically", () => {
+    const needle = 'NorThcOdErs';
+    const haystack = {
+      name: "Northcoders",
+      description: "Awesome coding bootcamp",
+      staff: {
+        tutors: "James and Chris",
+        support: "Louise"
+      },
+      contactDetails: {
+        web: "https://northcoders.com",
+        phone: "",
+        address: {
+          office: "Northcoders, Gold 67, The Sharp Project, Manchester",
+          postcode: "M40 5BJ"
+        }
+      },
+      reviews: {
+        april: {
+          chris: "I love Northcoders",
+          james: "It is awesome!"
+        },
+        may: {
+          louise: "Northcoders is a fantastic coding bootcamp"
+        }
+      }
+    };
+    expect(needleInHaystack(haystack, needle)).toEqual(["contactDetails.address.office", "contactDetails.web", "name", "reviews.april.chris", "reviews.may.louise"]);
+  });
 });
--- javascript-frontend-katas/katas/needleInHaystack/needleInHaystack.js
+++ javascript-frontend-katas/katas/needleInHaystack/needleInHaystack.js
@@ -1,16 +1,17 @@
 // Please do not change the name of this function
 const needleInHaystack = (haystack, needle, needleLocation = [], path = []) => {
-
   for (let key in haystack) {
-    const regex = new RegExp(`${needle}`, 'i')
-    if (typeof haystack[key] === "string" && (regex).test(haystack[key])) {
-      needleLocation.push(path.join(".") + `.${key}`);
+    const regex = new RegExp(`${needle}`, "i");
+    if (typeof haystack[key] === "string" && regex.test(haystack[key])) {
+      path.length > 0
+        ? needleLocation.push(path.join(".") + `.${key}`)
+        : needleLocation.push(key);
     } else if (typeof haystack[key] === "object") {
       needleInHaystack(haystack[key], needle, needleLocation, path.concat(key));
     }
   }
 
-  return needleLocation;
+  return needleLocation.sort();
 };
 
 module.exports = { needleInHaystack };
