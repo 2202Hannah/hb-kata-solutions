542dac3460f81dfa719b450b6bc96eddf087dce0
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-04-28 13:16:47 +0100

    refactor using regex to allow for both upper and lower case needles

--- javascript-frontend-katas/__tests__/needleInHaystack.test.js
+++ javascript-frontend-katas/__tests__/needleInHaystack.test.js
@@ -32,8 +32,39 @@ describe("needleInHaystack()", () => {
     };
     expect(needleInHaystack(haystack, needle)).toEqual([]);
   });
-  it("Returns the correct path for one instance of the needle when case in the same", () => {
+  it("Returns the correct path for one instance of the needle when case is the same", () => {
     const needle = "M40";
+    const haystack = {
+      name: "Northcoders",
+      description: "Awesome coding bootcamp",
+      staff: {
+        tutors: "James and Chris",
+        support: "Louise"
+      },
+      contactDetails: {
+        web: "https://northcoders.com",
+        phone: "",
+        address: {
+          office: "Northcoders, Gold 67, The Sharp Project, Manchester",
+          postcode: "M40 5BJ"
+        }
+      },
+      reviews: {
+        april: {
+          chris: "I love Northcoders",
+          james: "It is awesome!"
+        },
+        may: {
+          louise: "Northcoders is a fantastic coding bootcamp"
+        }
+      }
+    };
+    expect(needleInHaystack(haystack, needle)).toEqual([
+      "contactDetails.address.postcode"
+    ]);
+  });
+  it("Returns the correct path for one instance of the needle when case is different", () => {
+    const needle = "m40";
     const haystack = {
       name: "Northcoders",
       description: "Awesome coding bootcamp",
--- javascript-frontend-katas/katas/needleInHaystack/needleInHaystack.js
+++ javascript-frontend-katas/katas/needleInHaystack/needleInHaystack.js
@@ -1,7 +1,9 @@
 // Please do not change the name of this function
 const needleInHaystack = (haystack, needle, needleLocation = [], path = []) => {
+
   for (let key in haystack) {
-    if (typeof haystack[key] === "string" && haystack[key].includes(needle)) {
+    const regex = new RegExp(`${needle}`, 'i')
+    if (typeof haystack[key] === "string" && (regex).test(haystack[key])) {
       needleLocation.push(path.join(".") + `.${key}`);
     } else if (typeof haystack[key] === "object") {
       needleInHaystack(haystack[key], needle, needleLocation, path.concat(key));
