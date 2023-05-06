5d74141e88a0a3f9fc0df50b79dbf19fe69cf544
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-06 14:39:30 +0100

    add solution and tests for simplifiedArray()

--- javascript-frontend-katas/__tests__/simplifyDirections.test.js
+++ javascript-frontend-katas/__tests__/simplifyDirections.test.js
@@ -1,9 +1,22 @@
 const {
-  simplifyDirections,
-} = require('../katas/simplifyDirections/simplifyDirections');
+  simplifyDirections
+} = require("../katas/simplifyDirections/simplifyDirections");
 
-describe('simplifyDirections()', () => {
-  it('', () => {
-    // first test here
+describe("simplifyDirections()", () => {
+  it("returns an empty array when the directions cancel each other out", () => {
+    expect(simplifyDirections([`NORTH`, `SOUTH`])).toEqual([]);
+  });
+  it("returns [WEST] when the other directions cancel each other out", () => {
+    expect(simplifyDirections([`NORTH`, `SOUTH`, `WEST`])).toEqual(["WEST"]);
+  });
+  it("returns [WEST] when the other directions cancel each other out", () => {
+    expect(
+      simplifyDirections(["NORTH", "WEST", "SOUTH", "WEST", "EAST"])
+    ).toEqual(["WEST"]);
+  });
+  it("returns [NORTH, NORTH] when the other directions cancel each other out", () => {
+    expect(
+      simplifyDirections(["NORTH", "EAST", "SOUTH", "WEST", "NORTH", "NORTH"])
+    ).toEqual(["NORTH", "NORTH"]);
   });
 });
--- javascript-frontend-katas/katas/simplifyDirections/simplifyDirections.js
+++ javascript-frontend-katas/katas/simplifyDirections/simplifyDirections.js
@@ -1,6 +1,22 @@
 // Please do not change the name of this function
-const simplifyDirections = (directions) => {
-  // Your code here
-}
+const simplifyDirections = directions => {
+  const simplifiedArray = [];
 
-module.exports = { simplifyDirections }
+  const oppositesObject = {
+    NORTH: "SOUTH",
+    SOUTH: "NORTH",
+    EAST: "WEST",
+    WEST: "EAST"
+  };
+
+  directions.forEach(direction => {
+    if (simplifiedArray.includes(oppositesObject[direction])) {
+      const i = simplifiedArray.indexOf(oppositesObject[direction]);
+      simplifiedArray.splice(i, 1);
+    } else simplifiedArray.push(direction);
+  });
+
+  return simplifiedArray;
+};
+
+module.exports = { simplifyDirections };
