dbb67907828051482dbec00dcfb81557aa87d3bb
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-10 13:14:48 +0100

    add logic to add white space to justifyLine()

--- javascript-frontend-katas/__tests__/justifyLine.test.js
+++ javascript-frontend-katas/__tests__/justifyLine.test.js
@@ -6,4 +6,8 @@ describe("justifyLine()", () => {
       "String exceeds maximum line length."
     );
   });
+  it("adds an additional space between the first 2 words when the max line length is 1 more than the string length", () => {
+    const str = "foo foo foo foo";
+    expect(justifyLine(str, 16)).toBe("foo  foo foo foo");
+  });
 });
--- javascript-frontend-katas/katas/justifyLine/justifyLine.js
+++ javascript-frontend-katas/katas/justifyLine/justifyLine.js
@@ -3,6 +3,18 @@ const justifyLine = (str, maxLineLength) => {
   if (str.length > maxLineLength) {
     return "String exceeds maximum line length.";
   }
+
+  let extraSpace = maxLineLength - str.length;
+  const arr = str.split("");
+
+  arr.forEach((element, i) => {
+    if (extraSpace > 0 && element === " ") {
+      arr[i] += " ";
+      extraSpace--;
+    }
+  });
+
+  return arr.join("");
 };
 
 module.exports = { justifyLine };
