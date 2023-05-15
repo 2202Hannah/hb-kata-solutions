c99152b1262c47c3834bada6477c79f8a32e4ea0
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-15 22:09:24 +0100

    remove comments

--- javascript-frontend-katas/__tests__/csvParser.test.js
+++ javascript-frontend-katas/__tests__/csvParser.test.js
@@ -7,13 +7,13 @@ describe("csvParser()", () => {
       expect(csvParser(data)).toEqual([]);
     });
   });
-  // it("returns an array of objects if data is present in the csv file", () => {
-  //   fs.readFile("./data/cities.csv", "utf8", function(err, data) {
-  //     const parsedData = csvParser(data);
-  //     const firstKey = Object.keys(parsedData[0])
-  //     expect(firstKey.length).toNotBe(0);
-  //   });
-  // });
+  it("returns an array of objects if data is present in the csv file", () => {
+    fs.readFile("./data/cities.csv", "utf8", function(err, data) {
+      const parsedData = csvParser(data);
+      const firstKey = Object.keys(parsedData[0])
+      expect(firstKey.length).toNotBe(0);
+    });
+  });
   it("returns the expected array of objects for the cities.csv data", () => {
     fs.readFile("./data/cities.csv", "utf8", function(err, data) {
            exptect(csvParser(data)).toEqual([{city}])
