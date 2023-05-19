74bf91273659d73aa4f71716c4d07cc80457e65d
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-19 09:02:48 +0100

    add solution for multiplicationTable()

--- javascript-frontend-katas/__tests__/multiplicationTable.test.js
+++ javascript-frontend-katas/__tests__/multiplicationTable.test.js
@@ -7,14 +7,24 @@ describe("multiplicationTable()", () => {
     expect(multiplicationTable(0)).toEqual([]);
   });
   it("will return the expected outcome when n = 1", () => {
-    expect(multiplicationTable(1)).toEqual([[0, 1], [0, 1]]);
+    expect(multiplicationTable(1)).toEqual([[0, 1], [1, 1]]);
   });
   it("will return the expected outcome when n = 3", () => {
     expect(multiplicationTable(3)).toEqual([
       [0, 1, 2, 3],
-      [0, 0, 0, 0],
-      [0, 1, 2, 3],
-      [0, 2, 4, 6]
+      [1, 1, 2, 3],
+      [2, 2, 4, 6],
+      [3, 3, 6, 9]
+    ]);
+  });
+  it("will return the expected outcome when n = 5", () => {
+    expect(multiplicationTable(5)).toEqual([
+      [0, 1, 2, 3, 4, 5],
+      [1, 1, 2, 3, 4, 5],
+      [2, 2, 4, 6, 8, 10],
+      [3, 3, 6, 9, 12, 15],
+      [4, 4, 8, 12, 16, 20],
+      [5, 5, 10, 15, 20, 25]
     ]);
   });
 });
--- javascript-frontend-katas/katas/multiplicationTable/multiplicationTable.js
+++ javascript-frontend-katas/katas/multiplicationTable/multiplicationTable.js
@@ -3,18 +3,22 @@ const rangeInclusive = require("range-inclusive");
 // Please do not change the name of this function
 const multiplicationTable = num => {
   const arr = [];
+  let count = 0;
 
   for (let i = 0; i <= num; i++) {
-    arr.push(rangeInclusive(0, num));
+    arr.push(rangeInclusive(count, num + count));
+    count++;
   }
 
   arr.forEach((subArray, index) => {
-   if (index > 0) {
-     console.log(subArray.map((x, i) => x * i))
-   }
+    if (index > 0) {
+      subArray.forEach((number, i) => {
+        if (i > 0) {
+          subArray[i] = index * i;
+        }
+      });
+    }
   });
-
-  //console.log(arr)
 
   return num === 0 ? [] : arr;
 };
