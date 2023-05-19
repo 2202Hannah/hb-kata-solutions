c9f77d1f89eb36e500645c7b55d08d3339ffac8d
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-19 09:40:54 +0100

    add logic to pass second test for wrongWayCow()

--- javascript-frontend-katas/__tests__/wrongWayCow.test.js
+++ javascript-frontend-katas/__tests__/wrongWayCow.test.js
@@ -4,4 +4,7 @@ describe("wrongWayCow()", () => {
   it("will return an empty array if all the cows are facing the same way", () => {
     expect(wrongWayCow("cowcowcowcow")).toEqual([]);
   });
+  it("will return an empty array if all the cows are facing the same way", () => {
+    expect(wrongWayCow("wocwocwoccow")).toEqual([0, 3]);
+  });
 });
--- javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
+++ javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
@@ -1,6 +1,15 @@
 // Please do not change the name of this function
 const wrongWayCow = field => {
-  return [];
+  const arr = field.match(/.{3}/g);
+  const coordinateArray = [];
+
+  arr.forEach((cow, index) => {
+    if (cow !== arr[index -1] && (cow !== arr[index + 1] || cow !== arr[0])) {
+      coordinateArray.push(0, index)
+    }
+  })
+
+  return coordinateArray;
 };
 
 module.exports = { wrongWayCow };
