eb257f0c48cf0a1e2441c806d9d143de2fc83a47
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-19 10:20:35 +0100

    wrongWayCow work in progress

--- javascript-frontend-katas/__tests__/wrongWayCow.test.js
+++ javascript-frontend-katas/__tests__/wrongWayCow.test.js
@@ -7,4 +7,9 @@ describe("wrongWayCow()", () => {
   it("will return an empty array if all the cows are facing the same way", () => {
     expect(wrongWayCow("wocwocwoccow")).toEqual([0, 3]);
   });
+  it("will return an empty array if all the cows are facing the same way", () => {
+    const field = 'cowcowcow...\n...woccowcow\n............'
+    expect(wrongWayCow(field)).toEqual([1, 1]);
+  });
+  
 });
--- javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
+++ javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
@@ -1,14 +1,40 @@
 // Please do not change the name of this function
 const wrongWayCow = field => {
-  const arr = field.match(/.{3}/g);
+  const arr = field.split(`\n`);
+  const arr2 = [];
+  const arr3 = [];
+
+  arr.filter(a => a !== "...");
+
+  //console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    arr2.push(arr[i].match(/.{3}/g));
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    arr3.push(arr2[i].filter(a => a !== "..."));
+  }
+
+  // arr.forEach(line => {
+  //   arr[0] = line.match(/.{3}/g);
+  // });
+  // console.log(arr.map(line => {
+  //   line.match(/.{3}/g);
+  // }))
   const coordinateArray = [];
 
-  arr.forEach((cow, index) => {
-    if (cow !== arr[index -1] && (cow !== arr[index + 1] || cow !== arr[0])) {
-      coordinateArray.push(0, index)
-    }
-  })
+  //console.log(arr3);
 
+  arr3.forEach((line, index) => {
+    line.forEach((cow, i) => {
+      if (cow !== line[i - 1] && (cow !== line[i + 1] || cow !== line[0])) {
+        coordinateArray.push(index, i);
+      }
+    });
+  });
+
+  console.log(coordinateArray);
   return coordinateArray;
 };
 
