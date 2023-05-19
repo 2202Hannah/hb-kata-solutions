11736e4328005d1bc92dd2c3db4ef1a6e9add2e8
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-19 08:39:25 +0100

    multiplicationTable work in progress

--- javascript-frontend-katas/__tests__/multiplicationTable.test.js
+++ javascript-frontend-katas/__tests__/multiplicationTable.test.js
@@ -6,4 +6,15 @@ describe("multiplicationTable()", () => {
   it("will return an empty array when the n = 0", () => {
     expect(multiplicationTable(0)).toEqual([]);
   });
+  it("will return the expected outcome when n = 1", () => {
+    expect(multiplicationTable(1)).toEqual([[0, 1], [0, 1]]);
+  });
+  it("will return the expected outcome when n = 3", () => {
+    expect(multiplicationTable(3)).toEqual([
+      [0, 1, 2, 3],
+      [0, 0, 0, 0],
+      [0, 1, 2, 3],
+      [0, 2, 4, 6]
+    ]);
+  });
 });
--- javascript-frontend-katas/katas/multiplicationTable/multiplicationTable.js
+++ javascript-frontend-katas/katas/multiplicationTable/multiplicationTable.js
@@ -1,7 +1,22 @@
-// Please do not change the name of this function
-const multiplicationTable = (num) => {
-  const arr = []
-  return arr;
-}
+const rangeInclusive = require("range-inclusive");
 
-module.exports = { multiplicationTable }
+// Please do not change the name of this function
+const multiplicationTable = num => {
+  const arr = [];
+
+  for (let i = 0; i <= num; i++) {
+    arr.push(rangeInclusive(0, num));
+  }
+
+  arr.forEach((subArray, index) => {
+   if (index > 0) {
+     console.log(subArray.map((x, i) => x * i))
+   }
+  });
+
+  //console.log(arr)
+
+  return num === 0 ? [] : arr;
+};
+
+module.exports = { multiplicationTable };
