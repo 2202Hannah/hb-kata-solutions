bc03bd704dff3bc050fc7e2a5ce6ca22a05d12bf
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-21 13:47:35 +0100

    wrongWayCow kata first 4 tests passing

--- javascript-frontend-katas/__tests__/wrongWayCow.test.js
+++ javascript-frontend-katas/__tests__/wrongWayCow.test.js
@@ -4,12 +4,15 @@ describe("wrongWayCow()", () => {
   it("will return an empty array if all the cows are facing the same way", () => {
     expect(wrongWayCow("cowcowcowcow")).toEqual([]);
   });
-  it("will return an empty array if all the cows are facing the same way", () => {
+  it("will return expected coordinates for one line of cows", () => {
     expect(wrongWayCow("wocwocwoccow")).toEqual([0, 3]);
   });
-  it("will return an empty array if all the cows are facing the same way", () => {
+  it("will return expected coordinates when argument includes multiple lines of cows", () => {
     const field = 'cowcowcow...\n...woccowcow\n............'
     expect(wrongWayCow(field)).toEqual([1, 1]);
   });
-  
+  it("will return an expected coordinates when a line has one of each facing cow, ie second cow is wrong way cow", () => {
+    const field = 'cowcowcowcow\n......cowcow\n............\n...cow...woc'
+    expect(wrongWayCow(field)).toEqual([3, 3]);
+  });
 });
--- javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
+++ javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
@@ -2,22 +2,31 @@
 const wrongWayCow = field => {
   const arr = field.split(`\n`);
   const arr2 = [];
-  const arr3 = [];
-
-  arr.filter(a => a !== "...");
+  const coordinateArray = [];
 
   for (let i = 0; i < arr.length; i++) {
     arr2.push(arr[i].match(/.{3}/g));
   }
 
-  const coordinateArray = [];
-
   arr2.forEach((line, index) => {
+    let cowCounter = 0;
+    let wocCounter = 0;
     line.forEach((cow, i) => {
-      if (cow !== "..." && cow !== line[i - 1] && (cow !== line[i + 1])) {
+      if (cow === "cow") {
+        cowCounter += 1;
+      }
+      if (cow === "woc") {
+        wocCounter += 1;
+      }
+      if (cow !== "..." && cow !== line[i - 1] && cow !== line[i + 1]) {
         coordinateArray.push(index, i);
       }
     });
+
+    if (cowCounter === wocCounter && cowCounter > 0) {
+      coordinateArray.shift();
+      coordinateArray.shift();
+    }
   });
 
   return coordinateArray;
