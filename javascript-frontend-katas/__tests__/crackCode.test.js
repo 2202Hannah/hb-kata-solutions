73a194c4c888ada520233107126a7cfd110251b1
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-21 15:55:51 +0100

    add code and tests for crackCode kata

--- javascript-frontend-katas/__tests__/crackCode.test.js
+++ javascript-frontend-katas/__tests__/crackCode.test.js
@@ -1,7 +1,22 @@
-const { crackCode } = require('../katas/crackCode/crackCode');
+const { crackCode } = require("../katas/crackCode/crackCode");
 
-describe('crackCode()', () => {
-  it('', () => {
-    // first test here
+describe("crackCode()", () => {
+  it("returns true for code that passes the requirements", () => {
+    expect(crackCode("ddd-aaa-z-y-x-123(adxy)")).toBe(true);
+  });
+  it("returns true for code that passes the requirements", () => {
+    expect(crackCode('a-b-c-d-e-f-g-h-i-577(abcd)')).toBe(true);
+  });
+  it("returns true for code that meets the requirements", () => {
+    expect(crackCode('eeeeeeee-d-cc-b-aaaa(abce)')).toBe(true);
+  });
+  it("returns true for code that meets the requirements", () => {
+    expect(crackCode('fff-gg-xx-ss-yyy(fgsy)')).toBe(true);
+  });
+  it("returns false for code that fails the requirements", () => {
+    expect(crackCode('fff-gg-xx-ss-y(fgsy)')).toBe(false);
+  });
+  it("returns false for code that fails the requirements", () => {
+    expect(crackCode('fff-gg-xx-ss-y(fgsy)')).toBe(false);
   });
 });
--- javascript-frontend-katas/katas/crackCode/crackCode.js
+++ javascript-frontend-katas/katas/crackCode/crackCode.js
@@ -1,6 +1,22 @@
 // Please do not change the name of this function
-const crackCode = (encryptedKey) => {
-  // Your code here
-}
+const crackCode = encryptedKey => {
+  const letters = encryptedKey.slice(0, encryptedKey.indexOf("("));
+  const bracket = encryptedKey.slice(
+    encryptedKey.indexOf("(") + 1,
+    encryptedKey.length - 1
+  );
+  const code = [];
 
-module.exports = { crackCode }
+  letters
+    .split("-")
+    .sort((a, b) => b.length - a.length || a.localeCompare(b))
+    .forEach(element => {
+      if (/^([^0-9]*)$/.test(element) && code.length < 4) {
+        code.push(element[0]);
+      }
+    });
+
+  return code.sort().join("") === bracket;
+};
+
+module.exports = { crackCode };
