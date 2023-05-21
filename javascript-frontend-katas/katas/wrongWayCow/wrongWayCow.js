3a1816d1a510dc956ef610f685b6a0b8150a0e1b
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-21 13:03:18 +0100

    wrongWayCow() passing first 3 tests

--- javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
+++ javascript-frontend-katas/katas/wrongWayCow/wrongWayCow.js
@@ -6,35 +6,20 @@ const wrongWayCow = field => {
 
   arr.filter(a => a !== "...");
 
-  //console.log(arr);
-
   for (let i = 0; i < arr.length; i++) {
     arr2.push(arr[i].match(/.{3}/g));
   }
 
-  for (let i = 0; i < arr2.length; i++) {
-    arr3.push(arr2[i].filter(a => a !== "..."));
-  }
-
-  // arr.forEach(line => {
-  //   arr[0] = line.match(/.{3}/g);
-  // });
-  // console.log(arr.map(line => {
-  //   line.match(/.{3}/g);
-  // }))
   const coordinateArray = [];
 
-  //console.log(arr3);
-
-  arr3.forEach((line, index) => {
+  arr2.forEach((line, index) => {
     line.forEach((cow, i) => {
-      if (cow !== line[i - 1] && (cow !== line[i + 1] || cow !== line[0])) {
+      if (cow !== "..." && cow !== line[i - 1] && (cow !== line[i + 1])) {
         coordinateArray.push(index, i);
       }
     });
   });
 
-  console.log(coordinateArray);
   return coordinateArray;
 };
 
