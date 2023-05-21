f83a09bf8c669c51549b42b418ef2b49ff4aa11b
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-21 14:52:54 +0100

    add logic to getSubSquare

--- javascript-frontend-katas/__tests__/getSubSquare.test.js
+++ javascript-frontend-katas/__tests__/getSubSquare.test.js
@@ -16,7 +16,9 @@ describe("getSubSquare()", () => {
       [2, 8, 7, 4, 1, 9, 6, 3, 5],
       [3, 4, 5, 2, 8, 6, 1, 7, 9]
     ];
-    expect(getSubSquare(matrix, undefined, 0)).toBe("Please provide an xStartIndex");
+    expect(getSubSquare(matrix, undefined, 0)).toBe(
+      "Please provide an xStartIndex"
+    );
   });
   it("will return Please provide a yStartIndex if none is provided", () => {
     const matrix = [
@@ -30,7 +32,9 @@ describe("getSubSquare()", () => {
       [2, 8, 7, 4, 1, 9, 6, 3, 5],
       [3, 4, 5, 2, 8, 6, 1, 7, 9]
     ];
-    expect(getSubSquare(matrix, 0, undefined)).toBe("Please provide a yStartIndex");
+    expect(getSubSquare(matrix, 0, undefined)).toBe(
+      "Please provide a yStartIndex"
+    );
   });
   it("will return Subsquare exceeds the bounds of the matrix if they y or x start point is too high", () => {
     const matrix = [
@@ -44,6 +48,22 @@ describe("getSubSquare()", () => {
       [2, 8, 7, 4, 1, 9, 6, 3, 5],
       [3, 4, 5, 2, 8, 6, 1, 7, 9]
     ];
-    expect(getSubSquare(matrix, 7, 7)).toBe("Subsquare exceeds the bounds of the matrix");
+    expect(getSubSquare(matrix, 7, 7)).toBe(
+      "Subsquare exceeds the bounds of the matrix"
+    );
+  });
+  it("will return the expected 3x3 sub square", () => {
+    const matrix = [
+      [5, 3, 4, 6, 7, 8, 9, 1, 2],
+      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+      [1, 9, 8, 3, 4, 2, 5, 6, 7],
+      [8, 5, 9, 7, 4, 1, 4, 2, 3],
+      [4, 2, 6, 8, 5, 3, 7, 9, 1],
+      [7, 1, 3, 9, 2, 4, 8, 5, 6],
+      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+      [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ];
+    expect(getSubSquare(matrix, 0, 0)).toEqual([[5, 3, 4], [6, 7, 2], [1, 9, 8]]);
   });
 });
--- javascript-frontend-katas/katas/getSubSquare/getSubSquare.js
+++ javascript-frontend-katas/katas/getSubSquare/getSubSquare.js
@@ -1,16 +1,21 @@
 // Please do not change the name of this function
 const getSubSquare = (matrix, xStartIndex, yStartIndex) => {
-  let errorMessage = ""
-  const arr = []
-  
+  let errorMessage = "";
+  const arr = [];
+
   if (matrix === undefined) {
-    errorMessage = "Please provide a matrix"
+    errorMessage = "Please provide a matrix";
   } else if (xStartIndex === undefined) {
-    errorMessage = "Please provide an xStartIndex"
+    errorMessage = "Please provide an xStartIndex";
   } else if (yStartIndex === undefined) {
-    errorMessage = "Please provide a yStartIndex"
-  } else if (xStartIndex >= matrix.length - 2 || yStartIndex >= matrix.length - 2) {
-    errorMessage = "Subsquare exceeds the bounds of the matrix"
+    errorMessage = "Please provide a yStartIndex";
+  } else if (
+    xStartIndex >= matrix.length - 2 ||
+    yStartIndex >= matrix.length - 2
+  ) {
+    errorMessage = "Subsquare exceeds the bounds of the matrix";
+  } else for (let i = xStartIndex; i < xStartIndex + 3; i++) {
+    arr.push(matrix[i].slice(yStartIndex, yStartIndex + 3));
   }
 
   return errorMessage ? errorMessage : arr;
