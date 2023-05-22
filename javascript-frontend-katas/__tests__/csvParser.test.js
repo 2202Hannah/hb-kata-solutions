2821aa3303abf523a6200dd267d63ac6684a6dfa
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-22 08:57:57 +0100

    csvParser work in progress

--- javascript-frontend-katas/__tests__/csvParser.test.js
+++ javascript-frontend-katas/__tests__/csvParser.test.js
@@ -2,21 +2,27 @@ const { csvParser } = require("../katas/csvParser/csvParser");
 const fs = require("fs");
 
 describe("csvParser()", () => {
-  it("returns an empty array if no data is present in the csv file", () => {
+  it("returns an empty array if no data is present in the csv file", async () => {
+    const str = [];
     fs.readFile("./data/blank.csv", "utf8", function(err, data) {
-      expect(csvParser(data)).toEqual([]);
+      str.push(data);
     });
+    expect(await csvParser(str[0])).toEqual([]);
   });
-  it("returns an array of objects if data is present in the csv file", () => {
-    fs.readFile("./data/cities.csv", "utf8", function(err, data) {
-      const parsedData = csvParser(data);
-      const firstKey = Object.keys(parsedData[0])
+  it("returns an array of objects if data is present in the csv file", async () => {
+    const str = [];
+    await fs.readFile("./data/cities.csv", "utf8", function(err, data) {
+      //console.log(data)
+      str.push(data);
+      //console.log(str)
+    });
+    const parsedData = await csvParser(str[0]);
+      const firstKey = Object.keys(parsedData[0]);
       expect(firstKey.length).toNotBe(0);
-    });
   });
-  it("returns the expected array of objects for the cities.csv data", () => {
-    fs.readFile("./data/cities.csv", "utf8", function(err, data) {
-           exptect(csvParser(data)).toEqual([{city}])
- });
-})
-})
+  //   it("returns the expected array of objects for the cities.csv data", () => {
+  //     fs.readFile("./data/cities.csv", "utf8", function(err, data) {
+  //            exptect(csvParser(data)).toEqual([{city}])
+  //  });
+  // })
+});
--- javascript-frontend-katas/katas/csvParser/csvParser.js
+++ javascript-frontend-katas/katas/csvParser/csvParser.js
@@ -1,6 +1,7 @@
 // Please do not change the name of this function
 const csvParser = (csvString) => {
   const arr = [];
+  //console.log(csvString)
   return arr;
 }
 
