c1204ad234b28556e2e24bab58f8b6ad1acfdded
2202Hannah <hannahbarber22@hotmail.co.uk>
2023-05-22 10:24:56 +0100

    add logic so that 4 diagonal plays results in a win

--- javascript-frontend-katas/__tests__/checkDiagonalWinner.test.js
+++ javascript-frontend-katas/__tests__/checkDiagonalWinner.test.js
@@ -1,23 +1,44 @@
 const {
-  checkDiagonalWinner,
-} = require('../katas/checkDiagonalWinner/checkDiagonalWinner');
+  checkDiagonalWinner
+} = require("../katas/checkDiagonalWinner/checkDiagonalWinner");
 
-const {fillSquare} = require(`../katas/fillSquare/fillSquare`)
+const { fillSquare } = require(`../katas/fillSquare/fillSquare`);
 
-describe('checkDiagonalWinner()', () => {
-  it('returns false when there is no game play', () => {
-    const board = fillSquare([[null, null, null, null, null, null, null]])
-    board.pop()
-    expect(checkDiagonalWinner(board)).toBe(false)
+describe("checkDiagonalWinner()", () => {
+  it("returns false when there is no game play", () => {
+    const board = fillSquare([[null, null, null, null, null, null, null]]);
+    board.pop();
+    expect(checkDiagonalWinner(board)).toBe(false);
   });
-  it('returns false when there is no diagonal winner', () => {
-    const board = fillSquare([[], [], [], [], [], ['x','x','x','x'], []])
-    board.pop()
-    expect(checkDiagonalWinner(board)).toBe(false)
+  it("returns false when there is no diagonal winner", () => {
+    const board = fillSquare([[], [], [], [], [], ["x", "x", "x", "x"], []]);
+    board.pop();
+    expect(checkDiagonalWinner(board)).toBe(false);
   });
-  it('returns false when there is no diagonal winner', () => {
-    const board = fillSquare([[], [], [null, 'o'], [null, 'o'], [null, 'o'], [null, 'o'], []])
-    board.pop()
-    expect(checkDiagonalWinner(board)).toBe(false)
+  it("returns false when there is no diagonal winner", () => {
+    const board = fillSquare([
+      [],
+      [],
+      [null, "o"],
+      [null, "o"],
+      [null, "o"],
+      [null, "o"],
+      []
+    ]);
+    board.pop();
+    expect(checkDiagonalWinner(board)).toBe(false);
+  });
+  it("returns x when there x is the diagonal winner", () => {
+    const board = fillSquare([
+      [],
+      [],
+      [null, null, null, "x"],
+      [null, null, "x"],
+      [null, "x"],
+      ["x"],
+      []
+    ]);
+    board.pop();
+    expect(checkDiagonalWinner(board)).toBe("x");
   });
 });
--- javascript-frontend-katas/katas/checkDiagonalWinner/checkDiagonalWinner.js
+++ javascript-frontend-katas/katas/checkDiagonalWinner/checkDiagonalWinner.js
@@ -1,7 +1,23 @@
 // Please do not change the name of this function
-const checkDiagonalWinner = (board) => {
-  console.log(board)
-  return false;
-}
+const checkDiagonalWinner = board => {
+  const gamePlayArray = [];
+  let returnValue = false;
 
-module.exports = { checkDiagonalWinner }
+  board.forEach((row, index) => {
+    row.forEach((slot, i) => {
+      if (slot !== null) {
+        gamePlayArray.push(`${slot} ${index + i}`);
+      }
+    });
+  });
+
+  gamePlayArray.forEach((play, i) => {
+    if (gamePlayArray.filter(v => v === play).length > 3) {
+      returnValue = play[0];
+    }
+  });
+
+  return returnValue;
+};
+
+module.exports = { checkDiagonalWinner };
